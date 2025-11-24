5bf82b8cc9a19fc2d8e1381c75d4545958a6e759
Angelhpascual <Angelhpascual@gmail.com>
2025-11-24 18:24:29 +0100

    feat: add Zustand store for cart management including item manipulation and total calculations.

---
+++ src/ui/store/useCartStore.ts
@@ -0,0 +1,55 @@
+import type { CartItem } from "@domain/models/CartItem"
+import type { Product } from "@domain/models/Product"
+import { create } from "zustand"
+
+interface CartState {
+  items: CartItem[]
+  isOpen: boolean
+
+  addToCart(product: Product): void
+  removeFromCart(productId: number): void
+  toggleCart(): void
+  clearCart(): void
+
+  totalItems(): number
+  totalPrice(): number
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+  items: [],
+  isOpen: false,
+
+  addToCart: (product: Product) => {
+    const { items } = get()
+    const existingItem = items.find((i) => i.product.id === product.id)
+
+    if (existingItem) {
+      // Si ya existe, incrementamos cantidad
+      set({
+        items: items.map((i) =>
+          i.product.id === product.id ? { ...i, quantity: i.quantity + 1 } : i
+        ),
+      })
+    } else {
+      // Si no, lo añadimos con cantidad 1
+      set({ items: [...items, { product, quantity: 1 }] })
+    }
+  },
+
+  removeFromCart: (productId: number) => {
+    set({
+      items: get().items.filter((i) => i.product.id !== productId),
+    })
+  },
+
+  toggleCart: () => set({ isOpen: !get().isOpen }),
+  clearCart: () => set({ items: [] }),
+
+  totalItems: () => get().items.reduce((acc, item) => acc + item.quantity, 0),
+
+  totalPrice: () =>
+    get().items.reduce(
+      (acc, item) => acc + item.product.price.getValue() * item.quantity,
+      0
+    ),
+}))
